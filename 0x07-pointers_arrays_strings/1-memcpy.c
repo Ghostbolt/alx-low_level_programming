141f28f37bbf164d3b5c9f6b7db4f6e05ab69f87
Ghostbolt <lerza365@gmail.com>
2023-04-03 11:08:21 -0700

    memcpy verssion 1.0

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * *_memcpy - copy memory address
+ *
+ * @dest: stored memory address
+ * @src: copied memory address
+ * @n: value of bytes sum
+ *
+ * Return: copied memory results/outcome
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+
+}
