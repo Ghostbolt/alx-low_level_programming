2c7c727fa335fe37ac109a08a5bbae2754887fc7
Ghostbolt <lerza365@gmail.com>
2023-04-03 05:25:55 -0700

    memset for 0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset -  fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * @s: startup point
+ * @b: replacement character
+ * @n: bytes to be changed
+ *
+ * Return: changed results/outcome
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+
+	return(s);
+}
