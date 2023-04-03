daf45790de81c640d06edbb30681ac20de04dd30
Ghostbolt <lerza365@gmail.com>
2023-04-03 11:49:50 -0700

    im furious

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,8 @@
  * *_strchr - string character detective
  * @s: character carrier
  * @c: lost character
- * Return: returns 0 for success
+ *
+ * Return: always 0 for success
  */
 char *_strchr(char *s, char c)
 {
