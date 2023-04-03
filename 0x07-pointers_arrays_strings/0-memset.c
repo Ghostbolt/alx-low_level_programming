ad4fe84416c9f5cd005c28fd33372efc540c594e
Ghostbolt <lerza365@gmail.com>
2023-04-03 10:26:30 -0700

    version 2.1.01

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset -  fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * *_memset -  fills the first n bytes of the memory area pointed to by s with the constant byte b
  *
  * @s: startup point
  * @b: replacement character
