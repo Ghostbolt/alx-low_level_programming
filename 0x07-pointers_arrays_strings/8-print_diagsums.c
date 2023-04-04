dbda37e666c92ab5172fe2d4cba8c71e8a0b6a1a
Ghostbolt <lerza365@gmail.com>
2023-04-03 19:58:43 -0700

    sum inator

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - displays diagum
+ * @a: input
+ * @size: matrix limiter
+ * Return: voided, none
+ */
+void print_diagsums(int *a, int size)
+{
+	int i; 
+	unsigned int isum1 = 0, isum2 = 0;
+
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size + 1) == 0)
+			isum1 += *(a + i);
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			isum2 += *(a + i);
+	}
+
+	printf("%d, %d\n", isum1, isum2);
+}
