6e038a0f203e7dd903ad29d2e145d652ad3528fa
Ghostbolt <lerza365@gmail.com>
2023-04-10 15:29:28 -0700

    args the second

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,12 +4,12 @@
  * main - program name
  * @argc: arguemnts
  * @argv: arrays of args
- * Return: null
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	for (; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
