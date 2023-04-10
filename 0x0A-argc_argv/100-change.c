531a0822cbd2a8416808e5e27a5b5284d0eeb21a
Ghostbolt <lerza365@gmail.com>
2023-04-10 16:17:21 -0700

    que beuno

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,8 +3,7 @@
 #include "main.h"
 
 /**
- * main - prints the minimum number of coins to
- * make change for an amount of money
+ * main - prints the minimum n of coins
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -12,7 +11,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int num, j, result;
+	int iNum, i, iRes;
 	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
@@ -21,24 +20,23 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num = atoi(argv[1]);
-	result = 0;
+	iNum = atoi(argv[1]);
+	iRes = 0;
 
-	if (num < 0)
+	if (iNum < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	for (j = 0; j < 5 && num >= 0; j++)
+	for (i = 0; i < 5 && iNum >= 0; i++)
 	{
-		while (num >= coins[j])
+		while (iNum >= coins[i])
 		{
-			result++;
-			num -= coins[j];
+			iRes++;
+			iNum -= coins[i];
 		}
 	}
-
-	printf("%d\n", result);
+	printf("%d\n", iRes);
 	return (0);
 }
