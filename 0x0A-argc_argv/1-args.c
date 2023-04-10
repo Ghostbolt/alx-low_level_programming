40040a06e399de035664c04f59f8de0028034a0e
Ghostbolt <lerza365@gmail.com>
2023-04-10 15:10:56 -0700

    updated vers

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,11 +4,13 @@
  * main - program file
  * @argc: arguemnts
  * @argv: array of argcs
+ *
  * Return: null
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
+
 	printf("%d\n", argc - 1);
 
 	return(0);
