9d43369a187bfa26118acd20971e2f7e3e9170f7
Ghostbolt <lerza365@gmail.com>
2023-04-10 15:33:41 -0700

    args the sec

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main - program name
+ * main - program prints argcs
  * @argc: arguemnts
  * @argv: arrays of args
  * Return: 0
@@ -9,9 +9,12 @@
 int main(int argc, char *argv[])
 {
 	int i = 0;
+
 	for (i = 0; i < argc; i++)
 	{
+
 		printf("%s\n", argv[i]);
 	}
+
 	return(0);
 }
