e72a5a0bff297612f46ea34db8792feab66455cb
Ghostbolt <lerza365@gmail.com>
2023-04-11 15:52:35 -0700

    souka

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - create array of size x and assign c
+ * @size: size of arr
+ * @c: char
+ * Return: arr pointer, NULL if fail
+ **/
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	s = malloc(sizeof(char) * size);
+
+	if (size == 0 || s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+
+	return (s);
+}
