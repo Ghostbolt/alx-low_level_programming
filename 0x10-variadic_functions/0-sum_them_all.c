57e75f9402423b7743dccbc154be5c9c6c431765
Ghostbolt <lerza365@gmail.com>
2023-04-20 13:53:20 -0700

    riptide

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * iSum_them_all - parameter sum
+ * @n: parameter num
+ * @...: parameter var
+ * Return: n
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list _a;
+	unsigned int i, iSum = 0;
+
+	va_start(_a, n);
+
+	for (i = 0; i < n; i++)
+	{
+		iSum += va_arg(_a, int);
+	}
+	va_end(_a);
+
+	return (iSum);
+}
